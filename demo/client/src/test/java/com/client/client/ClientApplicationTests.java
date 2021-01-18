26217eef8da29ab9ae3c151ee00c6abba4b05cb7
ZC21GNL <72438913+GreenNeem@users.noreply.github.com>
2021-01-19 00:12:47 +0900

    SpringBoot
    learnig basic flow

---
+++ demo/client/src/test/java/com/client/client/ClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.client.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("zarchi!!");
+	}
+
+}
